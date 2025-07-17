4f1795d2f11ad2c54ca3a1924aa86d45f83f2478
Ivan <kowd.pauuh@gmail.com>
2025-07-17 11:18:10 +0000

    Implemented uuid_v4

---
+++ include/ttrack/uuid.hpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 Ivan Danylenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cstdlib>
+#include <string>
+
+namespace ttrack {
+
+std::string uuid_v4() {
+    static const char* hex_chars = "0123456789abcdef";
+    char uuid[37];
+
+    // generate uuid
+    for (int i = 0; i < 36; ++i) {
+        switch (i) {
+            case 8: case 13: case 18: case 23:
+                uuid[i] = '-';
+                break;
+            case 14:
+                uuid[i] = '4';
+                break;
+            case 19:
+                uuid[i] = hex_chars[(rand() % 4) + 8];
+                break;
+            default:
+                uuid[i] = hex_chars[rand() % 16];
+        }
+    }
+    uuid[36] = '\0';
+    return std::string(uuid);
+}
+
+}  // ttrack namespace
