c510c428281fae2a2d21681276c7a1ed8b590293
Ivan <kowd.pauuh@gmail.com>
2025-07-17 12:48:52 +0000

    Added save_experiment_metadata function

---
+++ include/ttrack/utils.hpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2025 Ivan Danylenko
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <filesystem>
+
+namespace ttrack {
+
+/**
+ * @brief Saves meta.yaml file under <logging_dir>/<uuid>.
+ * @param uuid Experiment UUID.
+ * @param experiment_name Experiment name to appear in meta.yaml.
+ * @param logging_dir Logging dir where to create an experiment.
+ */
+void save_experiment_metadata(
+    const std::string& uuid,
+    const std::string& experiment_name,
+    const std::string& logging_dir
+) {
+    // create experiment dir
+    std::string experiment_dir = logging_dir + "/" + uuid;
+    std::filesystem::create_directories(experiment_dir);
+
+    std::string meta_file_path = experiment_dir + "/meta.yaml";
+
+    int now = static_cast<unsigned int>(time(nullptr));
+
+    // open file
+    std::ofstream meta_file(meta_file_path);
+    if (!meta_file) {
+        std::cerr << "Failed to create file: " << meta_file_path << std::endl;
+        return;
+    }
+
+    // write metadata
+    meta_file << "artifact_location: mlflow-artifacts:/" << uuid << "\n";
+    meta_file << "creation_time: " << now << "\n";
+    meta_file << "experiment_id: '" << uuid << "'\n";
+    meta_file << "last_update_time: " << now << "\n";
+    meta_file << "lifecycle_stage: active\n";
+    meta_file << "name: " << experiment_name << "\n";
+
+    meta_file.close();
+}
+
+}  // ttrack namespace
